b10f98dbb3dc64ba331a4fce4eb1c11ad70342c9
jackarmstrong555 <joseph.s.armstrong@vanderbilt.edu>
2025-01-02 04:29:48 -0600

    another attempt to fix file issues

--- Scripts/VirtualNightSky/Packages/com.unity.render-pipelines.high-definition-config/CHANGELOG.md
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:61b369495e8bc9d282613222a1d557fba46e5610dcb9a7ff405568f919a30da8
-size 4128
--- Scripts/VirtualNightSky/Packages/com.unity.render-pipelines.high-definition-config/Runtime/ShaderConfig.cs.hlsl
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:9ce1fdf9883ab5eab205b5b456a7e1281d2377471ac69c295d28c69913377e49
-size 1270
